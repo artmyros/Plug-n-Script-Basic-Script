ee6ee74d28099caa9986abc457909d9e151001b9
Myroslav Artiukh <49280881+artmyros@users.noreply.github.com>
2024-04-28 07:30:24 +0200

    Add files via upload

---
+++ Basic Script.cxx
@@ -0,0 +1,24 @@
+string name="Basic Script";
+string description="You can build any Plug'n Script script on top of this basic script.";
+
+array<string> inputParametersNames = {};
+array<string> inputParametersUnits = {};
+array<string> inputParametersEnums = {};
+array<double> inputParameters( inputParametersNames.length );
+array<int> inputParametersSteps = {};
+array<double> inputParametersMin = {};
+array<double> inputParametersMax = {};
+
+void processBlock(BlockData& data)
+{
+	for(uint ch = 0; ch < audioOutputsCount; ch ++)
+	{
+		for(uint s = 0; s < data.samplesToProcess; s ++)
+		{
+			data.samples[ch][s] = data.samples[ch][s];
+		}
+	}
+}
+
+void updateInputParametersForBlock(const TransportInfo@ info){}
+
